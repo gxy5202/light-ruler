859c280eeb2eb2f79bd5aa8a21847503d9cc75c0
Gomi <37973863+gxy5202@users.noreply.github.com>
2021-11-01 09:43:37 +0800

    add files

---
+++ utils/getComputedStyle.ts
@@ -0,0 +1,19 @@
+/*
+ * @description: 获取元素样式
+ * @Author: Gouxinyu
+ * @Date: 2020-12-23 16:24:45
+ */
+export default function (target: HTMLElement | string, type: string): string {
+    if (typeof target === 'string' && window.getComputedStyle) {
+        const dom: HTMLElement = document.getElementById(target);
+        const style: CSSStyleDeclaration = window.getComputedStyle(dom, null);
+        return style[type];
+    }
+
+    if (target as HTMLElement && window.getComputedStyle) {
+        const style: CSSStyleDeclaration = window.getComputedStyle(target as HTMLElement, null);
+        return style[type];
+    }
+
+    throw new Error('Can not get target style');
+}
